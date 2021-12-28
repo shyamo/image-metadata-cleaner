dba649a9677412cb206d07a596e519233396b648
Shyam <bhadshyam@gmail.com>
2021-12-28 20:52:11 +1100

    fix: add counter for processed files

--- index.py
+++ index.py
@@ -6,9 +6,9 @@ from exif import Image
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-ERR_NO_FOLDER_PROVIDED = "WARNING: please provide folder with image(s) as a parameter"
+ERR_NO_FOLDER_PROVIDED = "WARNING: please provide folder with image(s) as a parameter."
 ERR_INVALID_FILE = "WARNING: Not an image: "
-SUCCESS_MSG = "\nProcess completed without errors"
+SUCCESS_MSG = "\nProcess completed without errors."
 
 def check_folder_provided():
     if len(sys.argv) > 1:
@@ -17,11 +17,11 @@ def check_folder_provided():
 def remove_image_metadata():
     print('Processing...\n')
     image_folder = sys.argv[1]
-
+    counter = 0
     try:
-        image_absolute_path = [item for item in (os.path.join(image_folder, folder) for folder in os.listdir(image_folder)) 
+        image_absolute_path_arr = [item for item in (os.path.join(image_folder, folder) for folder in os.listdir(image_folder)) 
             if os.path.isfile(item)]
-        for file in image_absolute_path:
+        for file in image_absolute_path_arr:
             filename = os.path.basename(file)
             if imghdr.what(filename, None):
                 image = Image.open(filename)
@@ -40,12 +40,14 @@ def remove_image_metadata():
                 cleaned_image = Image.new(image.mode, image.size)
                 cleaned_image.putdata(data)
                 cleaned_image.save(formatted_filename)
+                counter += 1
             else:
                 print(ERR_INVALID_FILE + filename)
     except NameError:
         print('error: ' + NameError)
     else:
-        print(SUCCESS_MSG)
+        PROCESS_COUNT = " Processed " +  str(counter) + " files."
+        print(SUCCESS_MSG + PROCESS_COUNT)
 
 def process_images():
     if check_folder_provided():
